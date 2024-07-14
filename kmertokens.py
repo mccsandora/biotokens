c666e60f7434bcb215a2f69fff18c97ce30b83f1
Tiana <tiananoll@gmail.com>
2024-07-14 12:40:05 -0700

    kmers w profiling

--- kmertokens.py
+++ kmertokens.py
@@ -6,7 +6,6 @@ from tokenizers import Tokenizer, models, trainers, pre_tokenizers, processors, 
 from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
-from concurrent.futures import ProcessPoolExecutor, as_completed
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -16,13 +15,18 @@ subsampled_genomes_dir = "subsampled_genomes"
 os.makedirs(subsampled_genomes_dir, exist_ok=True)
 os.makedirs(tokenizers_dir, exist_ok=True)
 
+try:
+    profile
+except NameError:
+    def profile(func):
+        return func
 
 def load_genome_sequence(filename):
     start_time = time.time()
     with open(os.path.join(genome_sequences_dir, filename), 'r') as f:
         genome = f.read().upper().replace('\n', '')
     end_time = time.time()
-    print(f"loaded the sequence in {end_time - start_time:.2f} secs")
+    print(f"loaded genome sequence in {end_time - start_time:.2f} secs")
     return genome
 
 def subsample_genome(genome, size=10**6):
@@ -34,10 +38,10 @@ def save_subsampled_genome(org, subsample):
         f.write(subsample)
     return subsample_path
 
-
 def generate_kmers(sequence, k):
     return [sequence[i:i+k] for i in range(len(sequence) - k + 1)]
 
+@profile
 def calculate_kmer_frequencies(sequence, k):
     start_time = time.time()
     kmer_freqs = defaultdict(int)
@@ -45,19 +49,22 @@ def calculate_kmer_frequencies(sequence, k):
         kmer = sequence[i:i+k]
         kmer_freqs[kmer] += 1
     end_time = time.time()
-    print(f"Calculated k-mer frequencies in {end_time - start_time:.2f} seconds")
+    print(f"calculated kmer frequencies in {end_time - start_time:.2f} secs")
     return kmer_freqs
 
+@profile
 def create_vocabulary(kmer_freqs, vocab_size):
     sorted_kmers = sorted(kmer_freqs.items(), key=lambda x: x[1], reverse=True)
     return [kmer for kmer, _ in sorted_kmers[:vocab_size]]
 
+@profile
 def calculate_compression(genome_sequence, tokens, vocab):
     total_length = len(genome_sequence)
     encoded_length = len(tokens)
-    vocab_length = sum(len(token.replace('##', '')) for token in vocab) 
+    vocab_length = sum(len(token.replace('##', '')) for token in vocab)  
     return (encoded_length + vocab_length) / total_length
 
+@profile
 def train_and_save_wordpiece_tokenizer(kmers, vocab_size, save_dir):
     start_time = time.time()
     tokenizer = Tokenizer(models.WordPiece(unk_token="[UNK]", max_input_chars_per_word=10**15))
@@ -75,13 +82,16 @@ def train_and_save_wordpiece_tokenizer(kmers, vocab_size, save_dir):
     print(f"trained and saved tokenizer for vocab size {vocab_size} in {end_time - start_time:.2f} secs")
     return save_path
 
+
+@profile
 def tokenize_file(genome_sequence, tokenizer):
     start_time = time.time()
     encoded = tokenizer.encode(genome_sequence)
     end_time = time.time()
-    print(f"Tokenized genome sequence in {end_time - start_time:.2f} seconds")
+    print(f"tokenized genome sequence in {end_time - start_time:.2f} secs")
     return encoded.tokens
 
+@profile
 def evaluate_vocab_size(kmer_freqs, genome_sequence, vocab_size, tokenizers_dir):
     start_time = time.time()
     vocab = create_vocabulary(kmer_freqs, vocab_size)
@@ -90,42 +100,29 @@ def evaluate_vocab_size(kmer_freqs, genome_sequence, vocab_size, tokenizers_dir)
     tokens = tokenize_file(genome_sequence, tokenizer)
     compression = calculate_compression(genome_sequence, tokens, vocab)
     end_time = time.time()
-    print(f"evaluated vocab size {vocab_size} in {end_time - start_time:.2f} secs")
+    print(f"Evaluated vocab size {vocab_size} in {end_time - start_time:.2f} seconds")
     return vocab_size, compression
 
-def heuristic_search_optimal_vocab_size(kmer_freqs, genome_sequence, initial_vocab_size=50, step_size=1000, max_vocab_size=80000, parallel=True):
+@profile
+def heuristic_search_optimal_vocab_size(kmer_freqs, genome_sequence, initial_vocab_size=50, step_size=1000, max_vocab_size=80000):
     vocab_sizes = list(range(initial_vocab_size, max_vocab_size + step_size, step_size))
     best_vocab_size = initial_vocab_size
     best_compression_factor = float('inf')
     compression_factors = []
 
-    if parallel:
-        with ProcessPoolExecutor() as executor:
-            futures = {executor.submit(evaluate_vocab_size, kmer_freqs, genome_sequence, vocab_size, tokenizers_dir): vocab_size for vocab_size in vocab_sizes}
-            for future in as_completed(futures):
-                try:
-                    vocab_size, compression = future.result()
-                    compression_factors.append((vocab_size, compression))
-                    print(f"Vocab size: {vocab_size}, compression factor: {compression}")
-                    if compression < best_compression_factor:
-                        best_compression_factor = compression
-                        best_vocab_size = vocab_size
-                except Exception as e:
-                    print(f"Error with vocab size {futures[future]}: {e}")
-    else:
-        for vocab_size in vocab_sizes:
-            try:
-                start_time = time.time()
-                vocab_size, compression = evaluate_vocab_size(kmer_freqs, genome_sequence, vocab_size, tokenizers_dir)
-                end_time = time.time()
-                print(f"evaluated vocab size {vocab_size} in {end_time - start_time:.2f} secs")
-                compression_factors.append((vocab_size, compression))
-                print(f"Vocab size: {vocab_size}, Compression factor: {compression}")
-                if compression < best_compression_factor:
-                    best_compression_factor = compression
-                    best_vocab_size = vocab_size
-            except Exception as e:
-                print(f"error w vocab size {vocab_size}: {e}")
+    for vocab_size in vocab_sizes:
+        try:
+            start_time = time.time()
+            vocab_size, compression = evaluate_vocab_size(kmer_freqs, genome_sequence, vocab_size, tokenizers_dir)
+            end_time = time.time()
+            print(f"evaluated vocab size {vocab_size} in {end_time - start_time:.2f} secs")
+            compression_factors.append((vocab_size, compression))
+            print(f"vocab size: {vocab_size}, compression factor: {compression}")
+            if compression < best_compression_factor:
+                best_compression_factor = compression
+                best_vocab_size = vocab_size
+        except Exception as e:
+            print(f"error w vocab size {vocab_size}: {e}")
 
     return best_vocab_size, best_compression_factor, compression_factors
 
@@ -143,11 +140,14 @@ if __name__ == "__main__":
 
     start_time = time.time()
     optimal_vocab_size, optimal_compression_factor, compression_factors = heuristic_search_optimal_vocab_size(
-        kmer_freqs, subsampled_sequence, initial_vocab_size=50, step_size=1000, max_vocab_size=80000, parallel=True)
+        kmer_freqs, subsampled_sequence, initial_vocab_size=50, step_size=500, max_vocab_size=100000)
     end_time = time.time()
-    print(f"search completed in {end_time - start_time:.2f} seconds")
+    print(f"search completed in {end_time - start_time:.2f} secs")
 
-    print(f"Optimal vocab size for {org}: {optimal_vocab_size}, compression factor: {optimal_compression_factor}")
+    print(f"optimal vocab size for {org}: {optimal_vocab_size}, compression factor: {optimal_compression_factor}")
+
+    for vocab_size, compression_factor in compression_factors:
+        print(f"vocab size: {vocab_size}, compression factor: {compression_factor}")
 
     vocab_sizes = [cf[0] for cf in compression_factors]
     compression_values = [cf[1] for cf in compression_factors]
@@ -160,4 +160,4 @@ if __name__ == "__main__":
 
     profiler.disable()
     stats = pstats.Stats(profiler).sort_stats('cumtime')
-    stats.print_stats(10)  
+    stats.print_stats(10) 
